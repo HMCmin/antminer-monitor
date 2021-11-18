3ba4064d00ddafe50ccfa18aa716f0d06ffdb1d0
HMCmin <88099641+HMCmin@users.noreply.github.com>
2021-11-18 17:13:56 +0100

    Update miner.py

--- antminermonitor/blueprints/asicminer/models/miner.py
+++ antminermonitor/blueprints/asicminer/models/miner.py
@@ -5,9 +5,10 @@ class Miner(Base):
     __tablename__ = 'miner'
     id = Column(Integer, primary_key=True)
     ip = Column(String(15), unique=True, nullable=False)
+    ips = Column(Integer, unique=True, nullable=False)
     model_id = Column(String(15), nullable=False)
     remarks = Column(String(255), nullable=True)
 
     def __repr__(self):
-        return "Miner(ip='{}', model_id='{}', remarks='{}')" \
-            .format(self.ip, self.model_id, self.remarks)
+        return "Miner(ip='{}', ips='{}', model_id='{}', remarks='{}')" \
+            .format(self.ip, self.ips, self.model_id, self.remarks)
